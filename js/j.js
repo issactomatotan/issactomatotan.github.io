d763f06fd9de181ba78713f111f887c9c2033a35
issac tomatotan <tomato082014@gmail.com>
2016-08-11 16:43:02 +0800

    1

--- js/j.js
+++ js/j.js
@@ -1,1 +1,21 @@
-console.log(123)
+(function(){
+  var a= document.getElementById('action'), xhr;
+  if(!a) return;
+  
+  function state(){
+    if(xhr.readyState == 4 && xhr.status == 200){
+      alert('do')
+    }
+  };
+  
+  function check(){
+    if(!window.XMLHttpRequest) return;
+    xhr= window.XMLHttpRequest;
+    
+    xhr.onreadystatechange= state;
+    xhr.open('GET','/act.html',true);
+    xhr.send(null)
+  };
+  
+  check()
+})();
